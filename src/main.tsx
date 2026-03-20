77f28f07d428ae5513deb70ed7c1fd694e5a9b8a
Michal Torma <miso.torma.dev@gmail.com>
2026-03-20 13:46:00 +0100

    feat: Load CSS before JS in index.html to improve initial rendering and reduce layout shifts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { GlobalErrorBoundary } from './components/GlobalErrorBoundary'
 import App from './App.tsx'
 
